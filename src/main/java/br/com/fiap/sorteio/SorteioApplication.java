0a0500569f47c8ec576fc14c00cd1f9993e35d02
olivanaa <oliv.anaclara@gmail.com>
2024-05-20 11:28:05 -0300

    2005

--- src/main/java/br/com/fiap/sorteio/SorteioApplication.java
+++ src/main/java/br/com/fiap/sorteio/SorteioApplication.java
@@ -2,6 +2,8 @@ package br.com.fiap.sorteio;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.info.Info;
@@ -18,5 +20,10 @@ public class SorteioApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(SorteioApplication.class, args);
 	}
+
+	@RequestMapping(value = "/", method = RequestMethod.GET)
+    public String home() {
+        return "redirect:/dado";
+    }
 
 }
